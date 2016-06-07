d5c724f9ed8d9201193ac927e3f149e8b4c21f69
jaspatz <januszdkn@mail.de>
2016-06-07 10:52:04 +0200

    Grundfunktionen programmiert: anzeige drehen, anzeige vershcieben, rechteck erstellen

--- qoglwidget.h
+++ qoglwidget.h
@@ -18,6 +18,10 @@ private:
     int xRot;
     int yRot;
     int zRot;
+    float xTran;
+    float yTran;
+
+    void drawQuad(float mx, float my, float mz, float width, float height, float length);
 
 protected:
     void initializeGL();
@@ -30,12 +34,14 @@ public slots:
     void setXRotation(int angle);
     void setYRotation(int angle);
     void setZRotation(int angle);
+    void onChangeTranslation(float dx, float dy);
 
 signals:
     void xRotationChanged(int angle);
     void yRotationChanged(int angle);
     void zRotationChanged(int angle);
     void changeRotation(int dx, int dy, int dz);
+    void changeTranslation(float dx, float dy);
 };
 
 #endif // QOGLWIDGET_H
