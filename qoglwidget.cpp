eb612b3f89abffa5510a3dd45ad3d77d712c471c
Janusz S. <15350276+janusz-sp@users.noreply.github.com>
2016-06-27 01:38:41 +0200

    collision funzt

--- qoglwidget.cpp
+++ qoglwidget.cpp
@@ -7,6 +7,7 @@
 
 int selectedCube = 0;
 bool rotationMode = false;
+bool gameStarted = false;
 
 QOGLWidget::QOGLWidget(QWidget *parent) : QOpenGLWidget(parent)
 {
@@ -17,23 +18,28 @@ QOGLWidget::QOGLWidget(QWidget *parent) : QOpenGLWidget(parent)
     yTran = 0.0;
     zTran = 0.0;
 
-    scale = 1.0;
+    scale = 0.5;
 
     timer_game = new QTimer(this);                              //Ein Spieltimer wird erstellt der in bestimmten intervallen das Spiel vorantreibt
-    timer_game->start(20);                                      //Timer soll alle 34 millisekunden auslösen (entspricht etwa 30 fps (17/1000 ~ 60fps))
+    timer_game->start(10);                                      //Timer soll alle 34 millisekunden auslösen (entspricht etwa 30 fps (17/1000 ~ 60fps))
 
 
-    ball = Sphere(QVector3D(0.0, 4.0, 0.0), 0.5, 0.5);               //Die Spielkugel wird erstellt
+    ball = Sphere(QVector3D(0.0, 15.0, 0.0), 0.5, 0.5);               //Die Spielkugel wird erstellt
     ball.setColor(0.2, 1.0, 0.8);
-    ball.setDirection(QVector3D(0.0, 0.1, 0.0));
+    ball.setDirection(QVector3D(0.0, 0.0, 0.0));
+
+    goal = Cylinder(QVector3D(0.0, -7.0, 0.0), 1.5, 3);
+    goal.setColor(0.7, 0.2, 0.7);
+
+    obstacle = Cube(QVector3D(0.0, 0.0, 0.0), 9, 6, 1.1);
+    obstacle.setColor(0.3, 0.3, 0.3);
 
     for(int i = 0; i < NR_CUBES; i++){
-        cube[i] = Cube(QVector3D((i)*4.25, -i*10.0, 0.0), 4.0, 1.0, 1.0);
+        cube[i] = Cube(QVector3D((i-2)*4.25, 6.0, 0.0), 4.0, 1.0, 1.0);
         cube[i].setRotation(0.0, 0.0, 0.0);
+        cube[i].setColor(0.7, 0.4, 0.2);
         if(i == selectedCube){
-            cube[i].setColor(0.0, 0.0, 1.0);
-        }else{
-            cube[i].setColor(0.5, 0.5, 0.5);
+            cube[i].setColor(1.0, 1.0, 1.0);
         }
     }
 
@@ -47,73 +53,104 @@ QOGLWidget::~QOGLWidget()
 
 void QOGLWidget::gameUpdate(){
 
-    QVector3D a = QVector3D(0.0, - GRAVITY, 0.0)/ball.getMass();        //Erdanziehungskraft
-    QVector3D luftreibung = - ball.getDirection().normalized() * 0.5 * 0.25 * ball.getArea() * ball.getDirection().lengthSquared(); //Luftwiderstand
+    if(gameStarted){
+        QVector3D gravitation = QVector3D(0.0, - GRAVITY, 0.0) * ball.getMass();        //Erdanziehungskraft
+        QVector3D luftreibung = - ball.getDirection().normalized() * REIBUNGSKONSTANTE * ball.getDirection().lengthSquared(); //Luftwiderstand
+        ball.setDirection(ball.getDirection() + gravitation + luftreibung);           //Neue geschwindigkeit ist alte Geschwindigkeit + Erdanziehungskraft + Reibungskraft
 
-    ball.setDirection(ball.getDirection() + a + luftreibung);           //Neue geschwindigkeit ist alte Geschwindigkeit + Erdanziehungskraft + Reibungskraft
+        QVector3D collision_point;
+        QVector3D collision_normal;
+        QVector3D sphere_newPosition = ball.getPos() + ball.getDirection();
+        float lambda;
 
-    for(int i = 0; i < 1; i++){
-        //alle cubes nach intersion testen, wenn sie intersionen, zeit ausrechnen bis zum intersionen und je nachdem welche zeit am geringsten ist, wird mit collision behandelt
-        checkCollision(cube[i], ball);
+        for(int i = 0; i < NR_CUBES; i++){ //alle cubes werden nach collision getestet
+            //alle cubes nach intersion testen, wenn sie intersionen, zeit ausrechnen bis zum intersionen und je nachdem welche zeit am geringsten ist, wird mit collision behandelt
+            if(checkCollision(cube[i], ball, collision_point, collision_normal, lambda)){
+                float geschwindigkeit = ball.getDirection().length();
+                QVector3D ballVelocityUnit = ball.getDirection().normalized();
+                QVector3D sphereNewVelocity;
+                if(geschwindigkeit <= 0.04){
+                    sphereNewVelocity = QVector3D(0.0, 0.0, 0.0);
+                }else{
+                    sphereNewVelocity = (2*(QVector3D::dotProduct(-ballVelocityUnit, collision_normal)) * collision_normal + ballVelocityUnit);
+                    sphereNewVelocity = sphereNewVelocity * geschwindigkeit;
+                }
+                sphere_newPosition = collision_point + lambda * sphereNewVelocity;
+                ball.setDirection(sphereNewVelocity);
+                cube[i].setColor(1, 0.7, 0.5);
+            }else{
+                cube[i].fadeToColor(0.7, 0.4, 0.2);
+            }
+        }
+
+        if(checkCollision(obstacle, ball, collision_point, collision_normal, lambda)){
+            float geschwindigkeit = ball.getDirection().length();
+            QVector3D ballVelocityUnit = ball.getDirection().normalized();
+            QVector3D sphereNewVelocity;
+            if(geschwindigkeit <= 0.04){
+                sphereNewVelocity = QVector3D(0.0, 0.0, 0.0);
+            }else{
+                sphereNewVelocity = (2*(QVector3D::dotProduct(-ballVelocityUnit, collision_normal)) * collision_normal + ballVelocityUnit);
+                sphereNewVelocity = sphereNewVelocity * geschwindigkeit;
+            }
+            sphere_newPosition = collision_point + lambda * sphereNewVelocity;
+            ball.setDirection(sphereNewVelocity);
+        }
+
+        ball.setPos(sphere_newPosition);
     }
-
     update();
 }
 
-void QOGLWidget::checkCollision(Cube& cube, Sphere& sphere){
+bool QOGLWidget::checkCollision(Cube& cube, Sphere& sphere, QVector3D& collision_point, QVector3D& collision_normal, float& lambda){ //könnte hiermit zusammenhängen
 
-    float lambda = 0;
-    QVector3D normal;
     QVector3D sphereOldPosition = sphere.getPos();
     QVector3D sphereOldVelocity = sphere.getDirection();
+    float radius = sphere.getRadius();
+    Plane planes[4];
 
-    QVector3D sphereNewPosition;
+    planes[0] = cube.getTopPlane();
+    planes[0].setPos(planes[0].getPos() + radius * planes[0].getNormal());
+    planes[1] = cube.getBottomPlane();
+    planes[1].setPos(planes[1].getPos() + radius * planes[1].getNormal());
+    planes[2] = cube.getRightPlane();
+    planes[2].setPos(planes[2].getPos() + radius * planes[2].getNormal());
+    planes[3] = cube.getLeftPlane();
+    planes[3].setPos(planes[3].getPos() + radius * planes[3].getNormal());
 
-    QVector3D planes[4];
-    QVector3D normals[4];
+    for(int i = 0; i < 4; i++){
+        QVector3D plane = planes[i].getPos();
+        QVector3D normal = planes[i].getNormal();
+        bool isIntersecting = checkIntersection(plane, normal, sphereOldPosition, sphereOldVelocity, lambda);
+        if(isIntersecting){
+            float distance_StartEnd = ((sphereOldPosition + sphereOldVelocity) - sphereOldPosition).length();
+            float distance_StartCol = ((sphereOldPosition + lambda*sphereOldVelocity) - sphereOldPosition).length();
 
-    QVector3D planes[0] = cube.getGlobalCoordinatesOfPoint(cube.getTop() + QVector3D(0.0, sphere.getRadius(), 0.0));
-    QVector3D normals[0] = cube.getGlobalCoordinatesOfVector(QVector3D(0.0, 1.0, 0.0));
+            if(distance_StartCol <= distance_StartEnd){
+                float lambdaToCollision = (distance_StartCol * lambda / distance_StartEnd);
 
-    bool isIntersecting = checkIntersection(plane_top, normal_top, sphereOldPosition, sphereOldVelocity, lambda);
+                collision_point = sphereOldPosition + lambdaToCollision * sphereOldVelocity;
 
-
-
-    normal = normal_top;
-
-
-    float distance_StartEnd = ((sphereOldPosition + sphereOldVelocity) - sphereOldPosition).length();
-    float distance_StartCol = ((sphereOldPosition + lambda*sphereOldVelocity) - sphereOldPosition).length();
-
-    sphereNewPosition = sphereOldPosition + sphereOldVelocity;
-
-    if(isIntersecting){
-        if(distance_StartCol <= distance_StartEnd){
-            qDebug("collision");
-            float geschwindigkeit = sphereOldVelocity.length();
-            float lambdaToCollision = (distance_StartCol * lambda/ distance_StartEnd);
-            float lambdaFromCollision = (1 - (distance_StartCol / distance_StartEnd))*lambda;
-
-            QVector3D intersectionPoint = sphereOldPosition + lambdaToCollision * sphereOldVelocity;
-
-            QVector3D ballVelocityUnit = sphereOldVelocity.normalized();
-            QVector3D sphereNewVelocity;
-            //if(geschwindigkeit <= 0.05){
-                //sphereNewVelocity = QVector3D(0.0, 0.0, 0.0);
-            //}else{
-                sphereNewVelocity = (2*(QVector3D::dotProduct(-ballVelocityUnit, normal)) * normal + ballVelocityUnit);
-                sphereNewVelocity = sphereNewVelocity * geschwindigkeit;
-            //}
-            sphereNewPosition = intersectionPoint + lambdaFromCollision * sphereNewVelocity;
-            sphere.setDirection(sphereNewVelocity);
+                if(isPointInCubePlane(collision_point, planes[i])){
+                    collision_normal = normal;
+                    lambda = (1 - (distance_StartCol / distance_StartEnd))*lambda;
+                    return true;
+                }
+            }
         }
     }
-    sphere.setPos(sphereNewPosition);
+
+    return false;
+}
+
+bool QOGLWidget::isPointInCubePlane(QVector3D point, Plane plane){
+    if(point.distanceToPoint(plane.getPos()) > (plane.getLength()/2.0)+ball.getRadius()){
+        return false;
+    }
+    return true;
 }
 
 bool QOGLWidget::checkIntersection(QVector3D plane_pos, QVector3D plane_normal, QVector3D sphere_pos, QVector3D sphere_direction, float &lambda){
-    /*
-    qDebug("is in");*/
 
     float skalarProdukt = QVector3D::dotProduct(sphere_direction, plane_normal);
 
@@ -123,7 +160,7 @@ bool QOGLWidget::checkIntersection(QVector3D plane_pos, QVector3D plane_normal, 
 
     lambda = (QVector3D::dotProduct(plane_normal, plane_pos - sphere_pos)/skalarProdukt);
 
-    if(lambda < 0){
+    if(lambda < 0){                         //Wenn Lambda < 0 ist dann fliegt die Kugel nicht richtung ebene
         return false;
     }
 
@@ -157,7 +194,7 @@ void QOGLWidget::paintGL()
         float delta = (2.0f * M_PI) / 45;
         float step = i*delta;
         float x = 15 * cosf(step);
-        float y = -5.0;
+        float y = -15.0;
         float z = 15 * sinf(step);
         glColor3f(1.0/i, (1.0/45.0) * i, 0.5);
         glVertex3f(x, y, z);
@@ -167,7 +204,9 @@ void QOGLWidget::paintGL()
     for(int i = 0; i < NR_CUBES; i++){
         cube[i].draw();
     }
+    obstacle.draw();
     ball.draw();
+    goal.draw();
 
     glPopMatrix();
     //----------------------------DRAW EVERYTHING
@@ -179,7 +218,7 @@ void QOGLWidget::initializeGL()
     initializeOpenGLFunctions();
 
     //Hintergrundfarbe wird gesetzt
-    glClearColor(0,0,0,1);
+    glClearColor(0.5,0.5,0.9,1);
 
     //Damit Transparenz gerendert werden kann
     glEnable(GL_BLEND);
@@ -224,33 +263,35 @@ void QOGLWidget::keyPressEvent(QKeyEvent *event)
 {
     switch(event->key()){
     case Qt::Key_Return:
-        ball.setDirection(QVector3D(0.0, 0.5, 0.0));
+        ball.setPos(QVector3D(0.0, 15.0, 0.0));
 
         if(selectedCube < NR_CUBES){
             if(!rotationMode){
                 rotationMode = true;
-                cube[selectedCube].setColor(0.0, 1.0, 0.0);
+                cube[selectedCube].fadeToColor(0.8, 0.8, 0.8);
             }else{
                 rotationMode = false;
-                cube[selectedCube].setColor(1.0, 0.0, 0.0);
+                cube[selectedCube].fadeToColor(0.6, 0.6, 0.6);
                 selectedCube++;
                 if(selectedCube < NR_CUBES){
-                    cube[selectedCube].setColor(0.0, 0.0, 1.0);
+                    cube[selectedCube].fadeToColor(1.0, 1.0, 1.0);
+                }else{
+                    gameStarted = true;
                 }
             }
         }
         break;
     case Qt::Key_Right:
-        xTran -= 0.05;
+        xTran -= 0.1;
         break;
     case Qt::Key_Left:
-        xTran += 0.05;
+        xTran += 0.1;
         break;
     case Qt::Key_Up:
-        yTran -= 0.05;
+        yTran -= 0.1;
         break;
     case Qt::Key_Down:
-        yTran += 0.05;
+        yTran += 0.1;
         break;
     }
 }
@@ -312,10 +353,10 @@ void QOGLWidget::changeTranslation(float dx, float dy)
 void QOGLWidget::changeZoom(float dzoom)
 {
     scale = scale + dzoom;
-    if(scale < 0.1){
-        scale = 0.1;
-    }else if(scale > 2.0){
-        scale = 2.0;
+    if(scale < 0.4){
+        scale = 0.4;
+    }else if(scale > 1.0){
+        scale = 1.0;
     }
 }
 
@@ -334,10 +375,10 @@ void QOGLWidget::changeRotation(float dx, float dy)
     if(xRot+dx < 0){
         xRot = 0;
     }
-    if(xRot+dx > 90){
-        xRot = 90;
+    if(xRot+dx > 70){
+        xRot = 70;
     }
-    if(xRot+dx > 0 && xRot+dx < 90){
+    if(xRot+dx > 0 && xRot+dx < 70){
         xRot = xRot + dx;
     }
 }
